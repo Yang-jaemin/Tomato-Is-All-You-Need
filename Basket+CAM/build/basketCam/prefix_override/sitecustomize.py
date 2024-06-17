caa4e80043ef12bdb7f7aa66a7404f00de81e05f
Juwon_J <77627794+ARDUINO33@users.noreply.github.com>
2024-06-17 16:13:56 +0900

    [add] jjw - Add Camera Project

---
+++ Basket+CAM/build/basketCam/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/juwon/ros2HW/Tomato-Is-All-You-Need/Basket+CAM/install/basketCam'
---
+++ Basket+CAM/build/basketCam/share/basketCam/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/juwon/ros2HW/Tomato-Is-All-You-Need/Basket+CAM/build/basketCam"
